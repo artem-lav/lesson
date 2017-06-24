fe0450e976cfeb04ddc1582e3487c1ffaeaa5236
Artem Aleksandrovich <lavren.artem@gmail.com>
2017-06-24 22:50:33 +0400

    Master commit 2

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 class Test{
 	protected $url;
 	protected $line;
+	protected $error;
 	
-	public function __construct($url,$line = 0){
+	public function __construct($url,$line = 1,$error){
 		$this->url = $url;
 		$this->line = $line;
+		$this->error = $error;
 	}
 }
